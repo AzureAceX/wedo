46d55f2087f7320ad0cc68534615231bebaeee88
Halliru Ibrahim <Urillah@gmail.com>
2021-02-23 23:11:58 +0800

    routing fix
    handing back from server to angular

--- src/main/java/com/urillah/wedo/WedoConfig.java
+++ src/main/java/com/urillah/wedo/WedoConfig.java
@@ -1,15 +1,42 @@
 package com.urillah.wedo;
 
+import java.io.IOException;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
-public class WedoConfig {
+public class WedoConfig implements WebMvcConfigurer{
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    registry.addResourceHandler("/**/*")
+        .addResourceLocations("classpath:/static/")
+        .resourceChain(true)
+        .addResolver(new PathResourceResolver() {
+            @Override
+            protected Resource getResource(String resourcePath,
+                Resource location) throws IOException {
+                  Resource requestedResource = location.createRelative(resourcePath);
+                  return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                : new ClassPathResource("/static/index.html");
+            }
+        });
+    }
+    
 	//Used for the entity to DTO mappings
 	@Bean
 	public ModelMapper modelMapper() {
 	    return new ModelMapper();
 	}
+	
+
+	
 }
--- src/main/java/com/urillah/wedo/controller/WedoController.java
+++ src/main/java/com/urillah/wedo/controller/WedoController.java
@@ -3,13 +3,14 @@ package com.urillah.wedo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class WedoController {
 
-	@GetMapping("/")
-	public String test() {
-		return "<h1> Hello From WeDoController </h1>";
-	}
+	// test mapping
+//	@GetMapping("/")
+//	public String test() {
+//		return "<h1> Hello From WeDoController </h1>";
+//	}
 	
 	
 }
