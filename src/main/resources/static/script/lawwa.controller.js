b8dba007ba9fa2c5592bb0e3eecfafd510a9bdc9
Halliru Ibrahim <Urillah@gmail.com>
2021-02-23 22:46:27 +0800

    Updating for cloud

--- src/main/resources/static/script/lawwa.controller.js
+++ src/main/resources/static/script/lawwa.controller.js
@@ -21,91 +21,87 @@ app.controller(
     $scope.sessionInfo.employeeId;
 
     //List Registry for registry view page - runs on app load
-    LawwaService.listRegistry(function (err, data) {
+    LawwaService.listTasks(function (err, data) {
       if (!err) {
-        $scope.registryList = data;
+        $scope.taskList = data;
       }
       //reconvert time back to simpler formats
     });
 
     //List Employees for employee view page - runs on app load
-    LawwaService.listEmployees(function (err, data) {
-      if (!err) {
-        $scope.employeesList = data;
-      }
-    });
+    // LawwaService.listEmployees(function (err, data) {
+    //   if (!err) {
+    //     $scope.employeesList = data;
+    //   }
+    // });
 
-    $.get("https://ipapi.co/json/", function (data) {
-      $scope.location = data.city;
-      // console.log(data)
-    });
 
     // Log in functionality
-    $scope.login = function () {
-      $scope.resetRegistryObj();
+    // $scope.login = function () {
+    //   $scope.resetRegistryObj();
 
-      //get employee using username/pass
-      if (!$scope.username || !$scope.password) {
-        alert("Please Enter Your Login Details");
-        return;
-      }
+    //   //get employee using username/pass
+    //   if (!$scope.username || !$scope.password) {
+    //     alert("Please Enter Your Login Details");
+    //     return;
+    //   }
 
-      LawwaService.getEmployeeAccount(
-        $scope.username,
-        $scope.password,
-        function (err, data) {
-          if (!err) {
-            $scope.logInObj.employee = data;
-            $scope.sessionInfo.employeeId = $scope.logInObj.employee.employeeId;
-            $scope.sessionInfo.signInTime = getDateTime();
-            $scope.completeLogin($scope.sessionInfo);
-          }
-        }
-      );
-    };
+    //   LawwaService.getEmployeeAccount(
+    //     $scope.username,
+    //     $scope.password,
+    //     function (err, data) {
+    //       if (!err) {
+    //         $scope.logInObj.employee = data;
+    //         $scope.sessionInfo.employeeId = $scope.logInObj.employee.employeeId;
+    //         $scope.sessionInfo.signInTime = getDateTime();
+    //         $scope.completeLogin($scope.sessionInfo);
+    //       }
+    //     }
+    //   );
+    // };
 
-    $scope.completeLogin = function (currentSessionInfo) {
-      //creates registry record for the sign in operation
-      $scope.logInObj.registry = {
-        employeeId: $scope.sessionInfo.employeeId,
-        isRemote: $scope.isRemote,
-        location: $scope.location,
-        signIn: $scope.sessionInfo.signInTime,
-        signOut: null,
-      };
+    // $scope.completeLogin = function (currentSessionInfo) {
+    //   //creates registry record for the sign in operation
+    //   $scope.logInObj.registry = {
+    //     employeeId: $scope.sessionInfo.employeeId,
+    //     isRemote: $scope.isRemote,
+    //     location: $scope.location,
+    //     signIn: $scope.sessionInfo.signInTime,
+    //     signOut: null,
+    //   };
 
       //REST For the registry creation using the object above
-      LawwaService.addRegistry($scope.logInObj.registry, function (err, data) {
-        if (!err) {
-          // alert("Sign In Completed");
-          window.location.href = "/loggedIn"; //forceful redirection - done because of lack of time
-        }
-        $scope.loggedIn = true;
-      });
-    };
+    //   LawwaService.addRegistry($scope.logInObj.registry, function (err, data) {
+    //     if (!err) {
+    //       // alert("Sign In Completed");
+    //       window.location.href = "/loggedIn"; //forceful redirection - done because of lack of time
+    //     }
+    //     $scope.loggedIn = true;
+    //   });
+    // };
 
-    $scope.logout = function () {
-      //find reisgtry from user sign in, and update that
+    // $scope.logout = function () {
+    //   //find reisgtry from user sign in, and update that
 
-      //find using sign in time and with eID?
-      LawwaService.getRegistry(function (err, data) {
-        if (!err) {
-          $scope.empRegistry = data;
-        }
-      });
+    //   //find using sign in time and with eID?
+    //   LawwaService.getRegistry(function (err, data) {
+    //     if (!err) {
+    //       $scope.empRegistry = data;
+    //     }
+    //   });
 
-      console.log("Employee Clocking Out At: " + getDateTime());
-      LawwaService.updateRegistry(function (err, data) {}, $scope.empRegistry);
-    };
+    //   console.log("Employee Clocking Out At: " + getDateTime());
+    //   LawwaService.updateRegistry(function (err, data) {}, $scope.empRegistry);
+    // };
 
-    $scope.createEmployee = function () {
-      LawwaService.createEmployee($scope.newEmp, function (err, data) {
-        if (!err) {
-          console.log($scope.newEmp);
-        }
-      });
-      $scope.closeModal();
-    };
+    // $scope.createEmployee = function () {
+    //   LawwaService.createEmployee($scope.newEmp, function (err, data) {
+    //     if (!err) {
+    //       console.log($scope.newEmp);
+    //     }
+    //   });
+    //   $scope.closeModal();
+    // };
 
     //   $scope.updateEmployee = function() {
     //     LawwaService.updateEmployee(function (err, data){
@@ -119,20 +115,20 @@ app.controller(
     /*
   Generate DATE / TIME for today
 */
-    function getDateTime() {
-      var today = new Date();
-      var date =
-        today.getFullYear() +
-        "-" +
-        (today.getMonth() + 1) +
-        "-" +
-        today.getDate();
-      var time =
-        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-      var dateTime = date + " " + time;
-      var res = new Date(dateTime).getTime();
-      return res;
-    }
+    // function getDateTime() {
+    //   var today = new Date();
+    //   var date =
+    //     today.getFullYear() +
+    //     "-" +
+    //     (today.getMonth() + 1) +
+    //     "-" +
+    //     today.getDate();
+    //   var time =
+    //     today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    //   var dateTime = date + " " + time;
+    //   var res = new Date(dateTime).getTime();
+    //   return res;
+    // }
 
     $scope.resetRegistryObj = function () {
       $scope.logInObj.registry = {};
