7b8a9ad9e515e55966379de0e88e429a5fe78468
Halliru Ibrahim <Urillah@gmail.com>
2021-02-25 00:12:30 +0800

    Update

--- src/main/java/com/urillah/wedo/controller/TaskController.java
+++ src/main/java/com/urillah/wedo/controller/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +57,19 @@ class TaskController {
 		}
 	}
 
+    @PutMapping(value = "/update-status")
+    public ResponseEntity<Task> udpateTaskStatus(@RequestBody Task taskDto) {
+    	Task taskObj = new Task();
+    	
+		if(taskRepositoryObj.findById(taskDto.getTaskid()).isPresent()) {
+			taskObj.setStatus(taskDto.getStatus());
+			taskRepositoryObj.save(taskObj);
+			return new ResponseEntity<>(taskObj, HttpStatus.OK);
+		}
+
+		return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+    }
+    
 //	@DeleteMapping(value = "/{accountId}")
 //	public ResponseEntity<HttpStatus> delete(@PathVariable("accountId") Long accountId) {
 //		try {
