35841878462985ad9fb7fcd439534c1b41f8a24b
Halliru Ibrahim <Urillah@gmail.com>
2021-02-25 00:37:34 +0800

    kill init

--- wedo/src/main/resources/static/script/app.js
+++ wedo/src/main/resources/static/script/app.js
@@ -1,25 +1,27 @@
 var app = angular.module("myApp", ['ngRoute']);
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
     
-  // if(window.history && window.history.pushState){
-  //   $locationProvider.html5Mode(true);
-  // }
+  if(window.history && window.history.pushState){
+    $locationProvider.html5Mode(true);
+  }
   
   $routeProvider
     .when("/", {
-      // templateUrl: "banana.html",
-      template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
+      templateUrl: "login.html",
     })
+    .when("/loggedIn", {
+        templateUrl: "loggedIn.html",
+      })
     .when("/tasks", {
       templateUrl: "tasks.html",
     })
-    // .when("/", {
-    //   templateUrl: "admin.registry.html",
-    // })
-    // .when("/banana", {
-    //   template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
-    // })
+    .when("/registry", {
+      templateUrl: "admin.registry.html",
+    })
+    .when("/banana", {
+      template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
+    })
     .otherwise({
       redirectTo: "/",
     });
