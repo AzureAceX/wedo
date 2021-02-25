1fb53f905d1e1dc94d8cdd314e8aad85007d69e0
Halliru Ibrahim <Urillah@gmail.com>
2021-02-26 05:23:51 +0800

    Update wedo.controller.js

--- src/main/resources/static/script/wedo.controller.js
+++ src/main/resources/static/script/wedo.controller.js
@@ -7,6 +7,7 @@ app.controller("WedoController",function ($scope, WedoSerivce, $location, $windo
     $scope.validTask = false;
     $scope.taskCount;
 
+
     //List tasks - to run on app load
     $scope.listTasks = function () {
       WedoSerivce.listTasks(function (err, data) {
@@ -18,6 +19,72 @@ app.controller("WedoController",function ($scope, WedoSerivce, $location, $windo
         }
       });
     };
+
+    //On page load
+    $scope.listTasks();
+
+      /***
+     * 
+     * Tabulator INIT
+     * 
+     * 
+     */
+
+//define data array
+var tabledata = [
+  {id:1, name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
+  {id:2, name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
+  {id:3, name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
+  {id:4, name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
+  {id:5, name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
+  {id:6, name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
+];
+
+// var tabledata = $scope.taskList;
+
+//Build Tabulator
+var table = new Tabulator("#tabulator-main", {
+  height:"311px",
+  selectable:true, //make rows selectable
+  columns:[
+    {title:"Task ID", field:"name", width:200},
+    {title:"Name", field:"progress", width:100, hozAlign:"right", sorter:"number"},
+    {title:"Description", field:"gender", width:100},
+    {title:"Status", field:"rating", hozAlign:"center", width:80},
+    // {title:"Favourite Color", field:"col"},
+    // {title:"Date Of Birth", field:"dob", hozAlign:"center", sorter:"date"},
+    // {title:"Driver", field:"car", hozAlign:"center", width:100},
+  ],
+  rowSelectionChanged:function(data, rows){
+      //update selected row counter on selection change
+    document.getElementById("select-stats").innerHTML = data.length;
+  },
+});
+
+$("#tabulator-main").tabulator("setData", tabledata);
+
+
+//select row on "select" button click
+document.getElementById("select-row").addEventListener("click", function(){
+  table.selectRow(1);
+});
+
+//deselect row on "deselect" button click
+document.getElementById("deselect-row").addEventListener("click", function(){
+  table.deselectRow(1);
+});
+
+//select row on "select all" button click
+document.getElementById("select-all").addEventListener("click", function(){
+  table.selectRow();
+});
+
+//deselect row on "deselect all" button click
+document.getElementById("deselect-all").addEventListener("click", function(){
+  table.deselectRow();
+});
+
+
 
     $scope.createTask = function () {
       if($scope.validateTask()){
@@ -80,76 +147,6 @@ app.controller("WedoController",function ($scope, WedoSerivce, $location, $windo
  
        return $scope.validTask;
      }
-
-    //On page load
-    $scope.listTasks();
-
-
-
-
-    /***
-     * 
-     * Tabulator INIT
-     * 
-     * 
-     */
-
-//define data array
-var tabledata = [
-  {id:1, name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
-  {id:2, name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
-  {id:3, name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
-  {id:4, name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
-  {id:5, name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
-  {id:6, name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
-];
-
-// var tabledata = $scope.taskList;
-
-//Build Tabulator
-var table = new Tabulator("tabulator-main", {
-  height:"311px",
-  selectable:true, //make rows selectable
-  columns:[
-    {title:"Task ID", field:"name", width:200},
-    {title:"Name", field:"progress", width:100, hozAlign:"right", sorter:"number"},
-    {title:"Description", field:"gender", width:100},
-    {title:"Status", field:"rating", hozAlign:"center", width:80},
-    // {title:"Favourite Color", field:"col"},
-    // {title:"Date Of Birth", field:"dob", hozAlign:"center", sorter:"date"},
-    // {title:"Driver", field:"car", hozAlign:"center", width:100},
-  ],
-  rowSelectionChanged:function(data, rows){
-      //update selected row counter on selection change
-    document.getElementById("select-stats").innerHTML = data.length;
-  },
-});
-
-//select row on "select" button click
-document.getElementById("select-row").addEventListener("click", function(){
-  table.selectRow(1);
-});
-
-//deselect row on "deselect" button click
-document.getElementById("deselect-row").addEventListener("click", function(){
-  table.deselectRow(1);
-});
-
-//select row on "select all" button click
-document.getElementById("select-all").addEventListener("click", function(){
-  table.selectRow();
-});
-
-//deselect row on "deselect all" button click
-document.getElementById("deselect-all").addEventListener("click", function(){
-  table.deselectRow();
-});
-
-
-
-
-
-
 
   }
 );
