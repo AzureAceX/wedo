f10c6f45b78feccaf0b90d519c6e96ee117ed2ce
AzureAceX <urillah@gmail.com>
2021-02-25 12:46:24 +0800

    injectorrr

--- src/main/resources/static/script/app.js
+++ src/main/resources/static/script/app.js
@@ -2,13 +2,13 @@ var app = angular.module('myApp', ['ngRoute']);
 
 
 
-app.config(['$routeProvider',
+app.config(['$routeProvider', '$locationProvider'
   function($routeProvider, $locationProvider) {
 
     if(window.history && window.history.pushState){
       $locationProvider.html5Mode(true);
     }
-    
+
     $routeProvider
       // .when('/', {
       // // templateUrl: ".html",
