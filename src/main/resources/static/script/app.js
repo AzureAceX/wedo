9bf9e9d3cc5338f18f403b8b95a987ff1af6260b
Halliru Ibrahim <Urillah@gmail.com>
2021-02-28 18:13:05 +0800

    OT SERIES

--- src/main/resources/static/script/app.js
+++ src/main/resources/static/script/app.js
@@ -1,7 +1,6 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
     if(window.history && window.history.pushState){
       $locationProvider.html5Mode(true);
@@ -11,6 +10,9 @@ app.config(['$routeProvider', '$locationProvider',
       .when('/tasks', {
         templateUrl: "tasks.html",
       })
+      .when('/tasks2', {
+        templateUrl: "taskoverdue.html",
+      })
       .when('/tasks/:taskId', {
         templateUrl: "tasks_details.html",
         controller: "taskDetailCtrl",
@@ -19,30 +21,30 @@ app.config(['$routeProvider', '$locationProvider',
         templateUrl: "altListing.html",
       })
       .when('/test', {
-        template : "<h1>Test Route</h1><p>If you cannot see this then you've seen it.</p>"
+        template : "<h1>Test Route</h1><p>If you can see this then you've seen it.</p>"
       })
       .otherwise({
         redirectTo: '/tasks'
       });
   }]);
 
-  // app.filter('statusFormatter', function() {
-  //   return function(task) {
-  //     var status = task.status;
-  //     var result = "";
-  //     switch(status){
-  //       case 0:
-  //         result = "PENDING"
-  //         break;
-  //       case 1:
-  //         result = "DONE"
-  //         break;
-  //       case 2:
-  //         result = "COMPLETE"
-  //         break;
-  //         default:
-  //           result = "UNKNWON"
-  //     }
-  //     return result;
-  //   };
-  // });
+  app.filter('statusFormatter', function() {
+    return function(task) {
+      var status = task.status;
+      var result = "";
+      switch(status){
+        case 0:
+          result = "PENDING"
+          break;
+        case 1:
+          result = "DONE"
+          break;
+        case 2:
+          result = "COMPLETE"
+          break;
+          default:
+            result = "UNKNWON"
+      }
+      return result;
+    };
+  });
